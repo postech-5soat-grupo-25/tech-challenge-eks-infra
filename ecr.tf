9f5cad0476a94788da55eb35b99343a79ccbff7d
Matheus Bachiste Lopes <matheusblopes10@gmail.com>
2024-05-12 10:31:55 -0300

    feat: update ecr

--- ecr.tf
+++ ecr.tf
@@ -1,10 +1,10 @@
-resource "aws_ecr_repository" "ecr_repository" {
+resource "aws_ecr_repository" "rust_ecr_repository" {
   name = "rust_api"
   image_tag_mutability = "MUTABLE" # Optional: Specify MUTABLE or IMMUTABLE
 }
 
 
-resource "aws_ecr_repository" "ecr_repository" {
+resource "aws_ecr_repository" "django_ecr_repository" {
   name = "django_api"
   image_tag_mutability = "MUTABLE" # Optional: Specify MUTABLE or IMMUTABLE
 }

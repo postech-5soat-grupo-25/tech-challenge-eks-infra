ef346e636e39ef1a7c64c474a3763b3068cd8150
Matheus Bachiste Lopes <matheusblopes10@gmail.com>
2024-05-12 21:56:29 -0300

    feat: use module to provide

--- eks.tf
+++ eks.tf
@@ -1,50 +1,100 @@
-# IAM role for eks
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
 
-resource "aws_iam_role" "tech_challenge_cluster_role" {
-  name = "tech-challenge-eks-cluster"
-  tags = {
-    tag-key = "tech-challenge-eks-cluster"
+# Filter out local zones, which are not currently supported 
+# with managed node groups
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.8.1"
+
+  name = "tech-challenge-vpc"
+
+  cidr = "10.0.0.0/16"
+  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
   }
 
-  assume_role_policy = <<POLICY
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Principal": {
-                "Service": [
-                    "eks.amazonaws.com"
-                ]
-            },
-            "Action": "sts:AssumeRole"
-        }
-    ]
-}
-POLICY
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
 }
 
-# eks policy attachment
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.5"
 
-resource "aws_iam_role_policy_attachment" "tech_challenge_cluster_policy" {
-  role       = aws_iam_role.tech_challenge_cluster_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
+  cluster_name    = "tech-challenge"
+  cluster_version = "1.29"
 
-# bare minimum requirement of eks
+  cluster_endpoint_public_access           = true
+  enable_cluster_creator_admin_permissions = true
 
-resource "aws_eks_cluster" "tech_challenge_cluster" {
-  name     = "tech_challenge"
-  role_arn = aws_iam_role.tech_challenge_cluster_role.arn
-
-  vpc_config {
-    subnet_ids = [
-      "subnet-0c3275aa09915a25c",
-      "subnet-00971f2717645e100",
-      "subnet-07a371f0c2f8dd740",
-      "subnet-04b66e840b6cbaac3"
-    ]
+  cluster_addons = {
+    aws-ebs-csi-driver = {
+      service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
+    }
   }
 
-  depends_on = [aws_iam_role_policy_attachment.tech_challenge_cluster_policy]
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_group_defaults = {
+    ami_type = "AL2_x86_64"
+
+  }
+
+  eks_managed_node_groups = {
+    one = {
+      name = "node-group-1"
+
+      instance_types = ["t2.small"]
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+    }
+
+    # two = {
+    #   name = "node-group-2"
+
+    #   instance_types = ["t2.small"]
+
+    #   min_size     = 1
+    #   max_size     = 2
+    #   desired_size = 1
+    # }
+  }
+}
+
+
+# https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/ 
+data "aws_iam_policy" "ebs_csi_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+module "irsa-ebs-csi" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version = "5.39.0"
+
+  create_role                   = true
+  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
+  provider_url                  = module.eks.oidc_provider
+  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
 }

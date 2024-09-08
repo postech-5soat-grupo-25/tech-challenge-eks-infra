c2aaf15daa9d54217606dc2087aeb2c1f5e4516f
MatheusBLopes <matheusblopes10@gmail.com>
2024-09-08 17:25:12 -0300

    feat: clear infra

--- eks.tf
+++ eks.tf
@@ -14,8 +14,8 @@ module "eks" {
     }
   }
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  vpc_id     = "vpc-0b7c1390b8189b953"
+  subnet_ids = ["subnet-0a4548cfbb5217c17", "subnet-047d29a707316bf5c", "subnet-002f2177f3837fccb"]
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"

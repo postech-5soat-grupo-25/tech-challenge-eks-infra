2f68c4a4c93c0679d9f2e7ae5de91006c834e861
MatheusBLopes <matheusblopes10@gmail.com>
2024-09-09 19:27:32 -0300

    fix: fix variables

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
-variable vpc_id {
+variable "vpc_id" {
   description = "Id da VPC na qual será criado o cluster"
   type        = string
   default     = "us-east-1"
 }
 
-variable subnets {
+variable "subnets" {
   description = "Ids das subnets para criar o cluster"
   type        = list(string)
   default     = []
